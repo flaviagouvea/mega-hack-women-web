53038658d523d666220fa3909648f4e5095d9b07
flaviagouvea <flaagouvea@gmail.com>
2020-09-01 22:28:29 -0300

    :rocket: My first deploy

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
