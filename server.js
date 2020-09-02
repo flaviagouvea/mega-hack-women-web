904cc54ecb534bba7c7a9cdb448b6a3b298eaf18
flaviagouvea <flaagouvea@gmail.com>
2020-09-01 22:33:43 -0300

    :rocket: My first deploy

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = 'Ninho';
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+app.use(express.static(__dirname + "build"));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
