0b046eeb26e2204078b4d18b9cf6b71ad969ec65
flaviagouvea <flaagouvea@gmail.com>
2020-09-01 22:37:40 -0300

    :rocket: My first deploy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(__dirname + "build"));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
