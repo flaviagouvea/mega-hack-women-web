43268e690f0b0372d8b7375a5e2f43451c3d05b3
flaviagouvea <flaagouvea@gmail.com>
2020-09-06 12:29:43 -0300

    feat(app): implement page and send email;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mega-hack-women-web';
+  message;
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      'user_email': new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')])
+    });
+  }
+
+
+  public sendEmail(e: Event) {
+    if (this.form.invalid) {
+      this.message = 'Email inválido';
+      return;
+    }
+
+    this.message = 'Email cadastrado!';
+    this.form.controls.user_email.setValue('');
+    /*emailjs.sendForm('ninhoapp', 'template_pu6pesi', e.target as HTMLFormElement, 'user_YWQ5G8qcxrqtmAVj6Eijl')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });*/
+  }
 }
